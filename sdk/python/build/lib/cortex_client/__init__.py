97047551590f710c60026c096e6739c4300c2a2b
sarptandoven <sarptan.doven1@gmail.com>
2026-07-07 11:44:18 -0700

    Drop leaked SDK build artifacts (build/, egg-info) + gitignore them

--- sdk/python/build/lib/cortex_client/__init__.py
+++
@@ -1,6 +0,0 @@
-"""cortex-client: a stdlib-only Python client for the Cortex local memory server."""
-
-from .client import CortexClient, CortexError
-
-__all__ = ["CortexClient", "CortexError"]
-__version__ = "0.1.0"
